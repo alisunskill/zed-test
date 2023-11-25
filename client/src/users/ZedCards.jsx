9fa589d652c9289130dcd4093e862b347af6f9d9
alisunskill <136700679+alisunskill@users.noreply.github.com>
2023-11-25 09:56:59 -0800

    Update ZedCards.jsx

--- client/src/users/ZedCards.jsx
+++ client/src/users/ZedCards.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Card from "react-bootstrap/Card";
+import "../zed.css"
 
 export default function ZedCards() {
   const [data, setData] = useState([]);
