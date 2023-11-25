137d3db62a4bf35e2490745fa40d4f38e2c00a2d
alisunskill <alighori267@gmail.com>
2023-11-25 04:07:12 -0800

    updated

---
+++ client/server/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const connectDB = require("./connect");
+const cors = require("cors");
+const updatedRoutes = require("./routes");
+
+const app = express();
+const PORT = 8000;
+
+// Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(cors({
+  origin: 'https://zed-frontend.vercel.app', 
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  optionsSuccessStatus: 204
+}));
+
+// Routes
+app.use("/api", updatedRoutes);
+
+const start = async () => {
+  try {
+    await connectDB(
+      "mongodb+srv://zedapp:zedapp@cluster0.nt0qqvj.mongodb.net/zedapp?retryWrites=true&w=majority"
+    );
+    console.log("MongoDB connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect to MongoDB:", error.message);
+  }
+};
+
+start();
